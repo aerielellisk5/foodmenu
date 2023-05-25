58d44396c14ba228f0689087c8110a06cb95ad8d
Aeriel <aerielellisk5@gmail.com>
2023-05-25 16:43:23 -0700

    have the app in a mostly working state, refresh doesnt work as intended

--- app.py
+++ app.py
@@ -1,78 +1,47 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+
+import psycopg2
 
 # initialize the app
 app = Flask(__name__)
 
 
+conn = psycopg2.connect(database="foodmenu", host="localhost", user="postgres", password="password123", port="5432")
+cur = conn.cursor()
 
+cur.execute('''CREATE TABLE IF NOT EXISTS meal (id serial PRIMARY KEY, mealname varchar(100), mealtype varchar(100));''' )
+conn.commit()
 
-ENV = 'dev'
-
-if ENV == 'dev':
-    app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password123@localhost:5432/foodmenu"
-    
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://username:password@localhost:port/DBNAME"
-    
-    # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://<username>:<password>@localhost/<database_name>"
-    
-    db = SQLAlchemy(app)
-    
-    
-
-    
-else:
-    app.debug =True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
-    
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-with app.app_context():
-        db.create_all()
-
-
-
-
-
-
-class Meals(db.Model):
-    __tablename__ = 'meal'
-    id = db.Column(db.Integer, primary_key=True)
-    mealname = db.Column(db.String(100), unique=True)
-    mealtype = db.Column(db.String(100))
-
-def __init__(self, mealname, mealtype):
-    self.mealname = mealname
-    self.mealtype = mealtype
-    
-    
-
+# when exactly is a good time to close the connection with the database?
+# Having trouble return back to the index page
 
 @app.route('/')
 def index():
     print("this actually works")
-    return render_template('index.html')
+    cur.execute('''SELECT * FROM meal''')
+    data = cur.fetchall()
+    return render_template('index.html', data = data)
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    print("hello")
     if request.method == 'POST':
         mealname = request.form['mealname']
         print("meal name is working")
+        print(type(mealname))
         mealtype = request.form['mealtype']
+        print(type(mealtype[0]))
         print("meal type is working")
-        # print("here is the print statement")
-        # print(mealtype, mealtype)
-        # how to get the print statement to show up in the console?
-        
-        
+
+        cur.execute("INSERT INTO meal (mealname, mealtype) VALUES (%s, %s)", (mealname, mealtype))
+        conn.commit()
         
         return render_template('nextpage.html')
+    
+
+
+
 
 if __name__ == '__main__':
     # we want the server to keep reloading since we are in developerment so we keep this to true
     # app.debug = True
-    app.run(host="0.0.0.0", port=3000)
+    app.run(port=3000)
